ff5e4776f2232b4a162f95e1831095ab7c56001b
Tuliany <tuliany.grande@amaceit.se>
2023-02-07 22:07:10 +0100

    added middleware

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,25 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:7000/', //proxy uri
+     ).then(function (response) {
+        console.log(response);
+      });
+  }, []);
+
   return (
+    
     <>
       <Head>
         <title>Create Next App</title>
